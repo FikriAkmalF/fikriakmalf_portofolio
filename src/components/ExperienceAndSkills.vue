ddee4f04ff81abc6055bbc80db77645f4f206999
FikriAkmalF <148306280+FikriAkmalF@users.noreply.github.com>
2025-10-30 09:19:54 +0700

    first commit

---
+++ src/components/ExperienceAndSkills.vue
@@ -0,0 +1,129 @@
+<template>
+    <section class="text-white mt-18" id="skills">
+        <div class="absolute right-0 top-[110rem] h-full w-full justify-end">
+            <span class="flex opacity-20">
+                <span class="w-16 h-32 rounded-l-full flex bg-primary blur-2xl"></span>
+                <span class="w-16 h-32 rounded-r-full flex bg-[#f88fc2] blur-2xl mt-14"></span>
+            </span>
+        </div>
+        <div class="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 xl:px-16">
+            <div class="mt-4 md:mt-0 text-left flex flex-col z-10 h-full w-[80%]">
+                <h2 class="text-4xl font-bold text-white text-left mb-4">My 
+                    <span class="text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary">Skills</span>
+                </h2>
+                <div class="mt-8" v-for="skill in Skills" :key="skill.id">
+                    <div class="flex items-end justify-between" data-aos="fade-right">
+                        <h4 class="font-semibold text-white">
+                            {{ skill.name }}
+                        </h4>
+                        <!-- <h3 class="text-2xl font-bold text-white">{{ skill.width }}</h3> -->
+                    </div>
+                    <!-- <div class="mt-2 h-1 w-full bg-[#131d30] rounded-full">
+                        <div class="h-1 rounded-full bg-primary" :style="`width :${skill.width}`"></div>
+                    </div> -->
+                </div>
+            </div>
+            <div data-aos="flip-left">
+                <h2 class="text-4xl font-bold text-white text-left mb-8 md:text-center md:mt-0 mt-8">My Experiences</h2>
+                <div class="space-y-8 py-8" data-aos="fade-left">
+                    <div v-for="element in Experinces" :key="element.id"
+                    class="flex  items-center rounded-xl p-4 bg-[#111a3e] shadow-lg border border-[#1f1641]"
+                    >
+                    <div class="w-1/4">
+                        <img src="https://img.icons8.com/ios-filled/100/ffffff/lawyer.png" alt="lawyer">
+                    </div>
+                    <div class="w-3/4 pl-4">
+                        <h3 class="text-2xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary">
+                            {{ element.role }}
+                        </h3>
+                        <p class="text-white">{{ element.company }}</p>
+                        <p class="text-white">{{ element.information }}</p>
+                        <p class="text-white">{{ element.date }}</p>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+<script setup>
+import { ref } from 'vue';
+const Skills =ref([
+    {
+        id:1,
+        name:'Laravel',
+        // width:'85%'
+    },
+    {
+        id:2,
+        name:'Vue JS',
+        // width:'70%'
+    },
+    {
+        id:3,
+        name:'React JS',
+        // width:'98%'
+    },
+    {
+        id:4,
+        name:'Tailwind CSS',
+        // width:'91%'
+    },
+    {
+        id:5,
+        name:'Bootstrap',
+        // width:'91%'
+    },
+    {
+        id:6,
+        name:'Google Fonts',
+        // width:'91%'
+    },
+    {
+        id:7,
+        name:'MySQL',
+        // width:'91%'
+    },
+    {
+        id:8,
+        name:'Problem Solving',
+        // width:'91%'
+    },
+    {
+        id:9,
+        name:'Canva',
+        // width:'91%'
+    },
+]);
+
+const Experinces =ref([
+    {
+        id:1,
+        role:'Full Stack Developer',
+        company:'Assyifa Islamic Philanthropy',
+        information:'Membuat Sistem Manajemen untuk mengatur seputar Zakat, Ziswaf dan Wakaf menggunakan Laravel',
+        date:'Februari - April 2023'
+    },
+    {
+        id:2,
+        role:'Full Stack Developer',
+        company:'Dinas Komunikasi dan Informatika',
+        information:'Project saat magang membuat Sistem Informasi Manajemen Rumah Sakit (SIMRS) RS Harmoni Kehidupan dengan menggunakan Laravel + Vue JS + Bootstrap + Midtrans',
+        date:'Mei - September 2024'
+    },
+    {
+        id:3,
+        role:'Full Stack Developer',
+        company:'Project tugas kampus',
+        information:'Membuat project secara berkelompok dengan orang nya di bagi beberapa bagian kepengurusan, kami membuat project untuk sewa Photographer Pernikahan secara online agar bisa booking dari jarak jauh yang dinamakan Go Photo dengan menggunakan PHP',
+        // date:'Mar 2022 - September 2023'
+    },
+    {
+        id:4,
+        role:'Full Stack Developer',
+        company:'Project untuk penyusunan Tugas Akhir',
+        information:'Membuat project untuk menyelesaikan Tugas Akhir dengan membuat sistem berbasis Chatbot AI Gemini menggunakan Laravel + React JS',
+        // date:'Mar 2022 - September 2023'
+    },
+])
+</script>
---
+++ src/components/ServicesSection.vue
@@ -0,0 +1,86 @@
+<template >
+    <section class="text-white mt-20" id="services">
+        <div class="px-4 xl:pl-16">
+            <h2 class="text-4xl font-bold text-white mb-4">My Skills</h2>
+        </div>
+        <div class="py-8 xl:px-16 px-4 sm:py-16 grid grid-cols-1 gap-6 pt-10 sm:grid-cols-2 md:gap-10 md:pt-12 lg:grid-cols-3">
+            <div v-for="service in services" :key="service.id" data-aos="fade-up"
+            class="px-8 py-12 rounded-xl bg-[#111a3e] shadow-lg border border-[#1f1641]"
+            >
+                <div class="mx-auto h-24 w-24 text-center xl:h-28 xl:w-28">
+                    <div>
+                        <img :src="service.icon" alt="service icon">
+                    </div>
+                </div>
+                <div class="text-center">
+                    <h3 class="pt-8 text-lg font-semibold text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary lg:text-xl">
+                        {{ service.name }}
+                    </h3>
+                    <p class="text-grey pt-4 text-sm text-justify xgroup-hover:text-white md:text-base">
+                        {{ service.description }}
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+<script setup>
+import {ref} from 'vue';
+const services =ref([
+    {
+        id:1,
+        icon:'https://img.icons8.com/ios-filled/100/ffffff/google-code.png',
+        name:'Full Stack Developer',
+        description:'Full Stack Developer adalah seseorang di bidang teknologi informasi yang memiliki kemampuan untuk mengembangkan website di semua lapisan dari awal hingga akhir. Mereka memiliki pemahaman yang mendalam tentang pengembangan front-end, pengembangan back-end, serta pengetahuan tentang database.'
+    },
+    {
+        id:2,
+        icon:'https://img.icons8.com/external-vectorslab-glyph-vectorslab/100/fffeee/external-Technical-Writing-mobile-app-development-vectorslab-glyph-vectorslab.png',
+        name:'Problem Solving',
+        description:'Proses berpikir dan mencari solusi untuk mengatasi suatu masalah dengan cara yang efektif dan efisien.'
+    },
+    {
+        id:3,
+        icon:'https://img.icons8.com/?size=100&id=1476&format=png&color=FFFFFF',
+        name:'HeidiSQL',
+        description:'Sistem manajemen basis data relasional (RDBMS) yang bersifat open-source dan menggunakan SQL (Structured Query Language) untuk mengelola data.'
+    },
+    {
+        id:4,
+        icon:'https://img.icons8.com/?size=100&id=rY6agKizO9eb&format=png&color=000000',
+        name:'Vue JS',
+        description:'Framework JavaScript progresif yang digunakan untuk membangun antarmuka pengguna (UI) dan aplikasi web interaktif.'
+    },
+    {
+        id:5,
+        icon:'https://img.icons8.com/?size=100&id=58811&format=png&color=FFFFFF',
+        name:'React JS',
+        description:'Framework JavaScript yang digunakan untuk membangun antarmuka pengguna (UI) yang interaktif dan efisien, terutama dalam aplikasi berbasis Single Page Application (SPA). Sebenarnya sama saja seperti Vue JS'
+    },
+    {
+        id:6,
+        icon:'https://img.icons8.com/?size=100&id=hUvxmdu7Rloj&format=png&color=000000',
+        name:'Laravel',
+        description:'Framework PHP yang digunakan untuk membangun aplikasi web dengan struktur yang rapi, aman, dan efisien. Bisa digabungkan dengan Vue JS / React JS jika digabung maka Laravel digunakan sebagai Backend'
+    },
+    {
+        id:7,
+        icon:'https://img.icons8.com/?size=100&id=4PiNHtUJVbLs&format=png&color=000000',
+        name:'Tailwind CSS',
+        description:'Framework CSS berbasis utility-first yang memungkinkan pengembang membangun desain antarmuka dengan cepat dan fleksibel. Biasa nya saat project digunakan untuk CSS di Vue JS / React JS'
+    },
+    {
+        id:8,
+        icon:'https://img.icons8.com/?size=100&id=PndQWK6M1Hjo&format=png&color=000000',
+        name:'Bootstrap',
+        description:'Framework CSS yang digunakan untuk membangun antarmuka web yang responsif dan cepat dengan komponen siap pakai. Biasa nya saat project digunakan untuk Laravel'
+    },
+    {
+        id:9,
+        icon:'https://img.icons8.com/?size=100&id=uY6QHPV8g5bd&format=png&color=000000',
+        name:'Google Fonts',
+        description:'Layanan gratis dari Google yang menyediakan berbagai jenis font web yang dapat digunakan dalam desain dan pengembangan website.'
+    },
+    ])
+</script>
+
